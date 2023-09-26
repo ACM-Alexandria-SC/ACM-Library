afeb8f483115ca2cf6f6e738c0d625ae1ba608af
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2023-09-27 00:15:07 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const bookTableBody = document.getElementById("bookTableBody");
+
+// function to populate the table with book data 
+function populateTable() {
+    books.forEach((book) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+                    <td style="text-align: left">${book.title}</td>
+                    <td><img src="images/${book.cover}" width="160" height="200"></td>
+                    <td>${book.authors.join("<br>")}</td>
+                    <td width="360" height="200" style="text-align: left">${book.description}</td>
+                    <td>${book.available ? "✅" : "❌"}</td>
+                `;
+        bookTableBody.appendChild(row);
+    });
+}
+
+populateTable();
+
+
+const submissionLink = document.getElementById("submissionLink");
+
+submissionLink.addEventListener("click", function (e) {
+    e.preventDefault();// prevent redirect to default
+    const googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLScUJJhHS3nSsYZo-6kHufEa7YuGw0dEIwOm_SUguR7xz3320A/viewform?usp=sf_link";
+    window.open(googleFormURL, "_blank", "noopener noreferrer");
+});
