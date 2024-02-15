fffc690b51c90f8ae79cac2af1f6688bcfff2420
ACM-Alexandria-SC <50934970+ACM-Alexandria-SC@users.noreply.github.com>
2024-02-15 17:23:29 +0200

    replace google sheets with json array

--- script.js
+++ script.js
@@ -1,39 +1,23 @@
 const bookTableBody = document.getElementById("bookTableBody");
-const sheetName = "booksheet";
-const url = "https://sheets.googleapis.com/v4/spreadsheets/";
-const sheetId = "1s1nqYpAwDQKK9cEucb-LGNiID-VkXd0zLThyhwTU_ko";
-const apiKey = "AIzaSyC55L8EqTgt1_LspLCTcGGc0I-7RyJhDpc";
-const range = "A2:E100";
 
-function callSheets(){
-    fetch(url + sheetId + "/values/" + sheetName + "!" + range + "?key=" + apiKey)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        populateTable(data.values);
+
+// function to populate the table with book data 
+function populateTable() {
+    books.forEach((book) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+                    <td style="text-align: left">${book.title}</td>
+                    <td><img src="${book.cover}" width="160" height="200"></td>
+                    <td>${book.authors.join("<br>")}</td>
+                    <td width="360" height="200" style="text-align: left">${book.description}</td>
+                    <td>${book.available ? "✅" : "❌"}</td>
+                `;
+        bookTableBody.appendChild(row);
     });
 }
 
-// function to populate the table with book data 
-function populateTable(data) {
-    data.forEach((book) => {
-        const row = document.createElement("tr");
-        // <td>${book[2].join("<br>")}</td>
-        row.innerHTML = `
-                    <td style="text-align: left">${book[0]}</td>
-                    <td><img src="${book[1]}" width="160" height="200"></td>
-                    <td>${book[2]}</td>
-                    <td width="360" height="200" style="text-align: left">${book[3]}</td>
-                    <td>${book[4]=='1' ? "✅" : "❌"}</td>
-                    `;
-                    bookTableBody.appendChild(row);
-                });
-}
-            
+populateTable();      
 
-callSheets();
 const submissionLink = document.getElementById("submissionLink");
 
 submissionLink.addEventListener("click", function (e) {
